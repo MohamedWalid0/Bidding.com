02306b71f1b5c098b0824ce53089bb8072402c67
MohamedWalid0 <mohamedw050@gmail.com>
2021-11-22 17:41:41 +0200

    prepare seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,14 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        $this->call([
+            RoleSeeder::class ,
+            GenderSeeder::class ,
+            CitySeeder::class ,
+            EventSeeder::class
+
+        ]);
+
     }
 }
