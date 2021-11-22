238971e8bcaf3dc1186eb80e0b8ea4419f28f2d9
mahmoud-adel44 <mhmoud.adel18@yahoo.com>
2021-11-22 22:08:39 +0200

    auth 2

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,35 +8,37 @@
             </div>
             <div class="card-body">
                 <p class="login-box-msg">Register a new membership</p>
-            {{ $errors }}
+
                 <form action="{{ route('register') }}" method="post">
                     @csrf
-
+                    @error('name')
+                    <span class="text-danger" role="alert">
+                            <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
                     <div class="input-group mb-3">
-                        <input id="name" type="text" class="form-control"
+                        <input id="name" type="text" class="form-control "
                                name="name"
                                value="{{ old('name') }}"  autocomplete="name" placeholder="Full Name">
-                        @error('name')
-                        <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                        @enderror
+
                         <div class="input-group-append">
                             <div class="input-group-text">
                                 <span class="fas fa-user"></span>
                             </div>
                         </div>
+
                     </div>
 
+                    @error('email')
+                        <span class="text-danger" role="alert">
+                                <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
                     <div class="input-group mb-3">
                         <input id="email" type="email" class="form-control "
                                name="email" value="{{ old('email') }}" required autocomplete="email"
                                placeholder="Email">
-                        @error('email')
-                        <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                        @enderror
+
                         <div class="input-group-append">
                             <div class="input-group-text">
                                 <span class="fas fa-envelope"></span>
@@ -45,15 +47,17 @@
                     </div>
 
                     {{-- phone --}}
+                    @error('phone')
+                        <span class="text-danger" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
                     <div class="input-group mb-3">
                         <input id="phone" type="number" class="form-control "
                                name="phone"
                                value="{{ old('phone') }}" required autocomplete="name" placeholder="phone">
-                        @error('phone')
-                        <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                        @enderror
+
+
                         <div class="input-group-append">
                             <div class="input-group-text">
                                 <span class="fas fa-user"></span>
@@ -62,16 +66,16 @@
                     </div>
 
                     {{-- address --}}
-
+                    @error('address')
+                    <span class="text-danger" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
                     <div class="input-group mb-3">
                         <input id="address" type="text" class="form-control "
                                name="address"
                                value="{{ old('address') }}" required autocomplete="address" placeholder="Address">
-                        @error('address')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                        @enderror
+
                         <div class="input-group-append">
                             <div class="input-group-text">
                                 <span class="fas fa-user"></span>
@@ -81,14 +85,15 @@
 
 
                     {{-- age --}}
-                    <div class="input-group mb-3">
-                        <input id="age" type="number" class="form-control" name="age"
-                               value="{{ old('age') }}" required autocomplete="name" placeholder="age">
-                        @error('age')
-                        <span class="invalid-feedback" role="alert">
+                    @error('age')
+                    <span class="text-danger" role="alert">
                             <strong>{{ $message }}</strong>
                         </span>
-                        @enderror
+                    @enderror
+                    <div class="input-group mb-3">
+                        <input id="age" type="number" class="form-control @error('age') is-invalid @enderror" name="age"
+                               value="{{ old('age') }}" required autocomplete="name" placeholder="age">
+
                         <div class="input-group-append">
                             <div class="input-group-text">
                                 <span class="fas fa-user"></span>
@@ -96,16 +101,16 @@
                         </div>
                     </div>
 
-
-                    <div class="input-group mb-3">
-                        <input id="password" type="password"
-                               class="form-control "
-                               name="password" required autocomplete="new-password" placeholder="Password">
-                        @error('password')
-                        <span class="invalid-feedback" role="alert">
+                    @error('password')
+                    <span class="text-danger" role="alert">
                             <strong>{{ $message }}</strong>
                         </span>
-                        @enderror
+                    @enderror
+                    <div class="input-group mb-3">
+                        <input id="password" type="password"
+                               class="form-control @error('password') is-invalid @enderror"
+                               name="password" required autocomplete="new-password" placeholder="Password">
+
                         <div class="input-group-append">
                             <div class="input-group-text">
                                 <span class="fas fa-lock"></span>
@@ -122,21 +127,18 @@
                         </div>
                     </div>
 
-
                     @error('city')
-                        <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
+                    <span class="text-danger" role="alert">
+                            <strong>{{ $message }}</strong>
                         </span>
                     @enderror
                     <div class="form-group">
 
-                        <select  class="custom-select rounded-0" id="city" name="city">
-                            <option selected hidden >Select your City</option>
+                        <select class="custom-select rounded-0" id="exampleSelectRounded0" name="city">
+                            <option selected hidden>Select your City</option>
                             @foreach (\App\Models\City::all() as $city)
                                 <option value="{{ $city->id }}">{{ $city->name }}</option>
                             @endforeach
-
-
 
                         </select>
 
@@ -146,7 +148,11 @@
 
                     <input type="hidden" name="role_id" value="1">
                     {{-- gender --}}
-
+                    @error('gender_id')
+                    <span class="text-danger" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
                     <div class="form-group">
                         <div class="form-check">
                             <input class="form-check-input" type="radio" name="gender_id" value="1">
@@ -158,11 +164,7 @@
                         </div>
 
                     </div>
-                    @error('gender_id')
-                    <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                    @enderror
+
 
                     {{-- city --}}
 
