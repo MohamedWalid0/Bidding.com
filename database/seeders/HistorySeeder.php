f86767a38dae474290937c8aba5bff7d7406c932
aminyasser <aminyasser88@gmail.com>
2021-12-08 10:36:16 +0200

    history factory and seeder

---
+++ database/seeders/HistorySeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\History;
+use Illuminate\Database\Seeder;
+
+class HistorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        History::factory(1000)->create();
+    }
+}
