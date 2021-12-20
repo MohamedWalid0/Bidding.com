cefdc67fe3b2c73a3c9cda4c4681d785e1f81061
aminyasser <aminyasser88@gmail.com>
2021-12-20 18:12:27 +0200

    uncomment bid observer

--- app/Observers/BidObserver.php
+++ app/Observers/BidObserver.php
@@ -12,68 +12,68 @@ use Symfony\Component\HttpFoundation\Response as Response;
 
 class BidObserver
 {
-//     // will check the product status before saving bids
-//     public function creating(Bid $bid)
-//     {
-//         $product = $bid->load('product')->product;
-//         abort_if($product->status === Product::INACTIVE, Response::HTTP_FORBIDDEN);
-//         abort_if($product->status === Product::ACTIVE && Carbon::now()->greaterThanOrEqualTo($product->deadline), Response::HTTP_FORBIDDEN);
-//     }
+    // will check the product status before saving bids
+    public function creating(Bid $bid)
+    {
+        $product = $bid->load('product')->product;
+        abort_if($product->status === Product::INACTIVE, Response::HTTP_FORBIDDEN);
+        abort_if($product->status === Product::ACTIVE && Carbon::now()->greaterThanOrEqualTo($product->deadline), Response::HTTP_FORBIDDEN);
+    }
 
-//     // will check the product deadline after saving bids
-//     public function created(Bid $bid)
-//     {
+    // will check the product deadline after saving bids
+    public function created(Bid $bid)
+    {
 
-//         $product = $bid->load('product')->product;
-//         $this->notify($product, $bid);
-//         if (Carbon::now()->diffInRealMinutes($product->deadline) < 60) {
-//             $product->updateQuietly([
-//                 'deadline' => $product->deadline->addHour()
-//             ]);
-//         }
-//     }
+        $product = $bid->load('product')->product;
+        $this->notify($product, $bid);
+        if (Carbon::now()->diffInRealMinutes($product->deadline) < 60) {
+            $product->updateQuietly([
+                'deadline' => $product->deadline->addHour()
+            ]);
+        }
+    }
 
-//     /**
-//      * @param $product
-//      * @param Bid $bid
-//      * @return void
-//      * notify all user related to this product
-//      */
-//     public function notify($product, Bid $bid): void
-//     {
-//         $users = $product->user_bids()
-//             ->where('user_id', '!=', $bid->user_id)
-//             ->get()
-//             ->merge($product->wishlists()->withoutGlobalScopes()->where('user_id', '!=', $bid->user_id)->get()->load('user')->pluck('user'));
-//         // this will be sent notification to all bidders
-//         Notification::send(
-//             $users,
-//             new NewBidAddedNotification($bid)
-//         );
-//         // this will be sent notification to the product owner
-//         $product->user->notify(new NewBidAddedNotification($bid));
-//     }
+    /**
+     * @param $product
+     * @param Bid $bid
+     * @return void
+     * notify all user related to this product
+     */
+    public function notify($product, Bid $bid): void
+    {
+        $users = $product->user_bids()
+            ->where('user_id', '!=', $bid->user_id)
+            ->get()
+            ->merge($product->wishlists()->withoutGlobalScopes()->where('user_id', '!=', $bid->user_id)->get()->load('user')->pluck('user'));
+        // this will be sent notification to all bidders
+        Notification::send(
+            $users,
+            new NewBidAddedNotification($bid)
+        );
+        // this will be sent notification to the product owner
+        $product->user->notify(new NewBidAddedNotification($bid));
+    }
 
 
-//     // will check the product status before updating bids
-//     public function updating(Bid $bid)
-//     {
-//         $product = $bid->load('product')->product;
-//         abort_if($product->status === Product::INACTIVE, Response::HTTP_FORBIDDEN);
-//         abort_if($product->status === Product::ACTIVE && Carbon::now()->greaterThanOrEqualTo($product->deadline), Response::HTTP_FORBIDDEN);
-//     }
+    // will check the product status before updating bids
+    public function updating(Bid $bid)
+    {
+        $product = $bid->load('product')->product;
+        abort_if($product->status === Product::INACTIVE, Response::HTTP_FORBIDDEN);
+        abort_if($product->status === Product::ACTIVE && Carbon::now()->greaterThanOrEqualTo($product->deadline), Response::HTTP_FORBIDDEN);
+    }
 
-// // will check the product deadline after updating bids
-//     public function updated(Bid $bid)
-//     {
-//         $product = $bid->load('product')->product;
-//         $this->notify($product, $bid);
-//         if (Carbon::now()->diffInRealMinutes($product->deadline) < 60) {
-//             $product->updateQuietly([
-//                 'deadline' => $product->deadline->addHour()
-//             ]);
-//         }
-//     }
+// will check the product deadline after updating bids
+    public function updated(Bid $bid)
+    {
+        $product = $bid->load('product')->product;
+        $this->notify($product, $bid);
+        if (Carbon::now()->diffInRealMinutes($product->deadline) < 60) {
+            $product->updateQuietly([
+                'deadline' => $product->deadline->addHour()
+            ]);
+        }
+    }
 
 
 }
