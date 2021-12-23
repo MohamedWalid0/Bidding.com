884031e6762fb23aaf657bb9fa3eaca9d74ffaf1
mahmoud-adel44 <mhmoud.adel18@yahoo.com>
2021-12-23 13:19:35 +0200

    fix show edit button to another user

--- app/Http/Controllers/Front/ProfileController.php
+++ app/Http/Controllers/Front/ProfileController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 use App\Models\Account;
 use App\Models\Rate;
 use App\Models\User;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class ProfileController extends Controller
@@ -22,20 +21,19 @@ class ProfileController extends Controller
     public function show(User $user)
     {
 
-        $rateCount = $user->rates()->count() ;
-        $ratingSum =  $user->rates()->sum('rate') ;
+        $rateCount = $user->rates()->count();
+        $ratingSum = $user->rates()->sum('rate');
 
-        $existsRate = Rate::where('user_id' , $user->id) ->
-                            where('rater_id' , Auth::user()->id)->first()  ; 
+        $existsRate = Rate::where('user_id', $user->id)->
+        where('rater_id', Auth::user()->id)->first();
 
-        if ( $rateCount > 0 ){
-            $userRate = $ratingSum/$rateCount ;
-        }
-        else{
-            $userRate = 0 ;
+        if ($rateCount > 0) {
+            $userRate = $ratingSum / $rateCount;
+        } else {
+            $userRate = 0;
         }
 
         $account = $user->account;
-        return view('front.profile.show' , compact('user' , 'account' , 'userRate' , 'rateCount' , 'existsRate'));
+        return view('front.profile.show', compact('user', 'account', 'userRate', 'rateCount', 'existsRate'));
     }
 }
